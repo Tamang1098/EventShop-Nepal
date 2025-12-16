377c72bbb8202c3904070c1249696255402238c9
suraj tamang <Tamang1098>
2025-12-16 10:20:34 +0545

    order added

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/cart', require('./routes/cart'));
+app.use('/api/orders', require('./routes/orders'));
 app.use('/api/payments', require('./routes/payments'));
 
 // Connect to MongoDB
